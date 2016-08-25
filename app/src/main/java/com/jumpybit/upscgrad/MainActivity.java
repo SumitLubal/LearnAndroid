1a68f16d32c85011b6faa1671bf7a470765c7117
Sumeet Lubal <Sumeet.Lubal@veritas.com>
2016-08-25 14:54:20 +0530

    Added Fetch WeatherTask and WeatherParser. This also lets you update UI

--- app/src/main/java/com/jumpybit/upscgrad/MainActivity.java
+++ app/src/main/java/com/jumpybit/upscgrad/MainActivity.java
@@ -1,5 +1,7 @@
 package com.jumpybit.upscgrad;
 
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.ListViewCompat;
@@ -9,7 +11,16 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.SimpleAdapter;
+import android.widget.Toast;
 
+import org.json.JSONException;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -17,6 +28,7 @@ public class MainActivity extends AppCompatActivity {
     private static final String TAG = MainActivity.class.getSimpleName();
     ArrayList<String> fakeData;
     ListViewCompat weather_list;
+    private ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,7 +39,8 @@ public class MainActivity extends AppCompatActivity {
         fakeData.addAll(Arrays.asList(a));
         a = null;
         weather_list = (ListViewCompat) findViewById(R.id.weather_list_view);
-        weather_list.setAdapter(new ArrayAdapter<String>(this, R.layout.weather_update_textview, R.id.weather_list_view_TextView, fakeData));
+        adapter = new ArrayAdapter<String>(this, R.layout.weather_update_textview, R.id.weather_list_view_TextView, fakeData);
+        weather_list.setAdapter(adapter);
     }
 
     @Override
@@ -40,10 +53,109 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
-        if(id == R.id.action_refresh){
-            Log.d(TAG,"Action Refresh clicked!!");
-            new FetchWeatherTask().execute();
+        if (id == R.id.action_refresh) {
+            Log.d(TAG, "Action Refresh clicked!!");
+            new FetchWeatherTask().execute(new String[]{"411046"});
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    public class FetchWeatherTask extends AsyncTask<String, String[], String[]> {
+        final private String TAG = FetchWeatherTask.class.getSimpleName();
+        private String postalCode = "411045";
+        private String modeType = "json";
+        private String unitType = "metric";
+        private String countForecast = "7";
+        private String appid = "dc31cf7c01ee0b4fe650e22953579996";
+
+        @Override
+        protected String[] doInBackground(String... params) {
+            // These two need to be declared outside the try/catch
+            // so that they can be closed in the finally block.
+            postalCode = params[0];
+            HttpURLConnection urlConnection = null;
+            BufferedReader reader = null;
+
+            // Will contain the raw JSON response as a string.
+            String forecastJsonStr = null;
+
+            try {
+                // Construct the URL for the OpenWeatherMap query
+                // Possible parameters are avaiable at OWM's forecast API page, at
+                // http://openweathermap.org/API#forecast
+                URL url = new URL("http://api.openweathermap.org/data/2.5/forecast/daily?q=94043&mode=json&units=metric&cnt=7&appid=dc31cf7c01ee0b4fe650e22953579996");
+                Uri.Builder builder = new Uri.Builder();
+
+
+                builder.scheme("http")
+                        .authority("api.openweathermap.org").appendPath("data").appendPath("2.5")
+                        .appendPath("forecast").appendPath("daily")
+                        .appendQueryParameter("q", postalCode).appendQueryParameter("mode", modeType)
+                        .appendQueryParameter("units", unitType)
+                        .appendQueryParameter("cnt", countForecast).appendQueryParameter("appid", appid);
+                Log.d(TAG, builder.toString());
+                // Create the request to OpenWeatherMap, and open the connection
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+
+                // Read the input stream into a String
+                InputStream inputStream = urlConnection.getInputStream();
+                StringBuffer buffer = new StringBuffer();
+                if (inputStream == null) {
+                    // Nothing to do.
+                    return null;
+                }
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    // Since it's JSON, adding a newline isn't necessary (it won't affect parsing)
+                    // But it does make debugging a *lot* easier if you print out the completed
+                    // buffer for debugging.
+                    buffer.append(line + "\n");
+                }
+
+                if (buffer.length() == 0) {
+                    // Stream was empty.  No point in parsing.
+                    return null;
+                }
+                forecastJsonStr = buffer.toString();
+                Log.d(TAG, forecastJsonStr);
+            } catch (IOException e) {
+                Log.e(TAG, "Error ", e);
+                // If the code didn't successfully get the weather data, there's no point in attemping
+                // to parse it.
+                return null;
+            } finally {
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+                if (reader != null) {
+                    try {
+                        reader.close();
+                    } catch (final IOException e) {
+                        Log.e(TAG, "Error closing stream", e);
+                    }
+                }
+            }
+            try {
+                return (new WeatherDataParser()).getWeatherDataFromJson(forecastJsonStr, Integer.parseInt(countForecast));
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String[] strings) {
+            if (strings != null) {
+                fakeData.clear();
+                fakeData.addAll(Arrays.asList(strings));
+                adapter.notifyDataSetChanged();
+            }else{
+                Toast.makeText(getApplicationContext(),"No internet access",Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
