1cc92d99da5d188738eb9b2fc74c271b1343631e
Sumeet Lubal <sumitlubal@hotmail.com>
2016-09-13 23:01:25 +0530

    Added ShareActionProvider for sharing text based message using intents. Intuiting user design Adjusted view of list adjusted strings translation

--- app/src/main/java/com/jumpybit/upscgrad/DetailActivity.java
+++ app/src/main/java/com/jumpybit/upscgrad/DetailActivity.java
@@ -2,16 +2,18 @@ package com.jumpybit.upscgrad;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.support.v7.widget.ShareActionProvider;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.TextView;
 
 public class DetailActivity extends AppCompatActivity {
+
+    private static final String TAG = "DetailACtivity", HASHTAG_FORECAST = "#LearnAndroid";
+    private String mForecastString;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,7 +21,8 @@ public class DetailActivity extends AppCompatActivity {
         setContentView(R.layout.activity_detail);
         Intent intent = getIntent();
         TextView t = (TextView) findViewById(R.id.detailtext_tv);
-        t.setText(intent.getStringExtra(Intent.EXTRA_TEXT));
+        mForecastString = intent.getStringExtra(Intent.EXTRA_TEXT);
+        t.setText(mForecastString);
         //getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
@@ -27,8 +30,25 @@ public class DetailActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.detail, menu);
+        MenuItem menuItem = menu.findItem(R.id.menu_item_share);
+        ShareActionProvider mShareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(menuItem);
+
+        if(mShareActionProvider != null){
+            mShareActionProvider.setShareIntent(createShareForecastIntent());
+        }else{
+            Log.d(TAG,"Share action provider is null ?");
+        }
         return true;
     }
+
+    private Intent createShareForecastIntent() {
+        Intent shareIntent = new Intent(Intent.ACTION_SEND);
+        shareIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
+        shareIntent.setType("text/plain");
+        shareIntent.putExtra(Intent.EXTRA_TEXT,mForecastString+ HASHTAG_FORECAST);
+        return shareIntent;
+    }
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
